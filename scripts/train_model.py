c907dd05be1fa3f48a5884d7caba3d680522ae32
Ali Çuğun <alicugun123@gmail.com>
2025-08-16 01:33:30 +0300

    update

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,20 +1,41 @@
+import torch
+import pandas as pd
+import numpy as np
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForSequenceClassification
+)
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+import os
+import json
+from tqdm import tqdm
+
+train = pd.read_csv("Data/train.csv")
+n_unique = train["label"].nunique()
+
 class BertTrainer:
+    def __init__(self, model_name = "dbmdz/bert-base-turkish-cased", num_labels = n_unique):
+        print(f"Model ekleniyor: {model_name}")
 
-    print("trainer class")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(f"Kullanılan cihaz: {self.device}")
+        
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    def yazdır():
-        print("yazdım")
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model_name,
+            num_labels = num_labels
+        )
 
-  
+        self.model.to(self.device)
+
+        print("Model yüklendi")
+
+         
+
 def main():
-    print("main")
-
-trainer = BertTrainer
-
-trainer.yazdır()
-
- 
+    trainer = BertTrainer()
     
-    
-if __name__ == "__main" :
-    main()   
+if __name__ == "__main__":
+    main()
+
